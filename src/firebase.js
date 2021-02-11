b10e20f56c36bb6ae9b60f87a170f23fbc2c78fb
Ananye Singhal <ananyesinghal@gmail.com>
2021-02-11 16:37:01 +0530

    starter

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAXonHJavZjge_vvFYiEJXC3H3Wy5YEn7Q",
+    authDomain: "movies-007.firebaseapp.com",
+    projectId: "movies-007",
+    storageBucket: "movies-007.appspot.com",
+    messagingSenderId: "579898889451",
+    appId: "1:579898889451:web:186cd8e63c3133ac811aae",
+    measurementId: "G-5106D7P14N"
+  };
